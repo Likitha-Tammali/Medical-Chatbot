bfe7cc2a32b82a7859bb6b3a3f21c1a4db53162d
likitha <likithathammali7819@gmail.com>
2025-05-10 15:05:39 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Likitha',
+    author_email='likithathammali7819@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
